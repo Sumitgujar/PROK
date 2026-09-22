f875d92f7b644ddf2c959a694001b5a9138d8509
Sumitgujar <sumitgujar1604@gmail.com>
2026-09-22 23:51:49 +0530

    updated core app & constants

--- mobile/lib/core/app_router.dart
+++ mobile/lib/core/app_router.dart
@@ -1,21 +1,21 @@
-
-import "package:flutter/material.dart";
-import "package:prok_mobile/core/constants.dart";
-import "package:prok_mobile/screens/splash_screen.dart";
-import "package:prok_mobile/screens/login_screen.dart";
-import "package:prok_mobile/screens/student/home_screen.dart";
-import "package:prok_mobile/screens/student/ask_prok_screen.dart";
-import "package:prok_mobile/screens/student/attendance_screen.dart";
-import "package:prok_mobile/screens/student/documents_screen.dart";
-import "package:prok_mobile/screens/student/scholarships_screen.dart";
-import "package:prok_mobile/screens/student/courses_screen.dart";
-import "package:prok_mobile/screens/student/notifications_screen.dart";
-import "package:prok_mobile/screens/student/profile_screen.dart";
-import "package:prok_mobile/screens/teacher/todays_classes_screen.dart";
-import "package:prok_mobile/screens/teacher/class_details_screen.dart";
-import "package:prok_mobile/screens/teacher/mark_attendance_screen.dart";
-import "package:prok_mobile/screens/teacher/attendance_history_screen.dart";
-
+import 'package:flutter/material.dart';
+import 'package:prok_mobile/core/constants.dart';
+import 'package:prok_mobile/screens/splash_screen.dart';
+import 'package:prok_mobile/screens/login_screen.dart';
+import 'package:prok_mobile/screens/student/home_screen.dart';
+import 'package:prok_mobile/screens/student/ask_prok_screen.dart';
+import 'package:prok_mobile/screens/student/attendance_screen.dart';
+import 'package:prok_mobile/screens/student/documents_screen.dart';
+import 'package:prok_mobile/screens/student/scholarships_screen.dart';
+import 'package:prok_mobile/screens/student/courses_screen.dart';
+import 'package:prok_mobile/screens/student/notifications_screen.dart';
+import 'package:prok_mobile/screens/student/profile_screen.dart';
+import 'package:prok_mobile/screens/teacher/todays_classes_screen.dart';
+import 'package:prok_mobile/screens/teacher/class_details_screen.dart';
+import 'package:prok_mobile/screens/teacher/mark_attendance_screen.dart';
+import 'package:prok_mobile/screens/teacher/attendance_history_screen.dart';
+import 'package:prok_mobile/screens/teacher/attendance_insights_screen.dart';
+import 'package:prok_mobile/screens/teacher/teacher_profile_screen.dart';
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     Widget page;
@@ -31,9 +31,11 @@ class AppRouter {
       case AppRoutes.studentNotifications: page = const StudentNotificationsScreen(); break;
       case AppRoutes.studentProfile: page = const StudentProfileScreen(); break;
       case AppRoutes.teacherHome: page = const TeacherTodaysClassesScreen(); break;
-      case "/teacher/class-details": page = const ClassDetailsScreen(); break;
-      case "/teacher/mark-attendance": page = const MarkAttendanceScreen(); break;
-      case "/teacher/attendance-history": page = const AttendanceHistoryScreen(); break;
+      case AppRoutes.teacherClassDetails: page = const ClassDetailsScreen(); break;
+      case AppRoutes.teacherMarkAttendance: page = const MarkAttendanceScreen(); break;
+      case AppRoutes.teacherAttendanceHistory: page = const AttendanceHistoryScreen(); break;
+      case AppRoutes.teacherAttendanceInsights: page = const AttendanceInsightsScreen(); break;
+      case AppRoutes.teacherProfile: page = const TeacherProfileScreen(); break;
       default: page = const SplashScreen();
     }
     return MaterialPageRoute(builder: (_) => page, settings: settings);
--- mobile/lib/core/constants.dart
+++ mobile/lib/core/constants.dart
@@ -1,22 +1,19 @@
-
-class ApiConfig {
-  static const String baseUrl = String.fromEnvironment(
-      "API_BASE_URL", defaultValue: "http://10.0.2.2:8000");
-}
-
+class ApiConfig { static const String baseUrl = String.fromEnvironment('API_BASE_URL', defaultValue: 'http://10.0.2.2:8000'); }
 class AppRoutes {
-  static const String splash = "/";
-  static const String login = "/login";
-  static const String studentHome = "/student/home";
-  static const String studentAskProk = "/student/ask-prok";
-  static const String studentAttendance = "/student/attendance";
-  static const String studentDocuments = "/student/documents";
-  static const String studentScholarships = "/student/scholarships";
-  static const String studentCourses = "/student/courses";
-  static const String studentNotifications = "/student/notifications";
-  static const String studentProfile = "/student/profile";
-  static const String teacherHome = "/teacher/home";
-  static const String teacherMarkAttendance = "/teacher/mark-attendance";
-  static const String teacherAttendanceHistory = "/teacher/attendance-history";
-  static const String teacherClassDetails = "/teacher/class-details";
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String studentHome = '/student/home';
+  static const String studentAskProk = '/student/ask-prok';
+  static const String studentAttendance = '/student/attendance';
+  static const String studentDocuments = '/student/documents';
+  static const String studentScholarships = '/student/scholarships';
+  static const String studentCourses = '/student/courses';
+  static const String studentNotifications = '/student/notifications';
+  static const String studentProfile = '/student/profile';
+  static const String teacherHome = '/teacher/home';
+  static const String teacherClassDetails = '/teacher/class-details';
+  static const String teacherMarkAttendance = '/teacher/mark-attendance';
+  static const String teacherAttendanceHistory = '/teacher/attendance-history';
+  static const String teacherAttendanceInsights = '/teacher/insights';
+  static const String teacherProfile = '/teacher/profile';
 }
