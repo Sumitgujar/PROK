8f15ec3fa8652b8377433b60e7142d6ac9fffcb6
Sumitgujar <sumitgujar1604@gmail.com>
2026-09-22 23:47:58 +0530

    updated .dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,21 +1,20 @@
-
-import "package:flutter/material.dart";
-import "package:provider/provider.dart";
-import "package:prok_mobile/core/app_router.dart";
-import "package:prok_mobile/core/constants.dart";
-import "package:prok_mobile/providers/auth_provider.dart";
-import "package:prok_mobile/providers/attendance_provider.dart";
-import "package:prok_mobile/providers/document_provider.dart";
-import "package:prok_mobile/providers/scholarship_provider.dart";
-import "package:prok_mobile/providers/course_provider.dart";
-import "package:prok_mobile/providers/notification_provider.dart";
-import "package:prok_mobile/providers/ai_chat_provider.dart";
-
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:prok_mobile/core/app_router.dart';
+import 'package:prok_mobile/core/constants.dart';
+import 'package:prok_mobile/theme/app_theme.dart';
+import 'package:prok_mobile/providers/auth_provider.dart';
+import 'package:prok_mobile/providers/attendance_provider.dart';
+import 'package:prok_mobile/providers/document_provider.dart';
+import 'package:prok_mobile/providers/scholarship_provider.dart';
+import 'package:prok_mobile/providers/course_provider.dart';
+import 'package:prok_mobile/providers/notification_provider.dart';
+import 'package:prok_mobile/providers/ai_chat_provider.dart';
 void main() => runApp(const ProkApp());
-
 class ProkApp extends StatelessWidget {
   const ProkApp({super.key});
-  @override Widget build(BuildContext context) => MultiProvider(
+  @override
+  Widget build(BuildContext context) => MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => AuthProvider()),
       ChangeNotifierProvider(create: (_) => AttendanceProvider()),
@@ -26,17 +25,10 @@ class ProkApp extends StatelessWidget {
       ChangeNotifierProvider(create: (_) => AiChatProvider()),
     ],
     child: MaterialApp(
-      title: "PROK",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E3A8A)),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF1E3A8A),
-          foregroundColor: Colors.white,
-          elevation: 0),
-      ),
+      title: 'PROK', debugShowCheckedModeBanner: false,
+      theme: ProkTheme.light,
       initialRoute: AppRoutes.splash,
       onGenerateRoute: AppRouter.generateRoute,
-    ));
+    ),
+  );
 }
