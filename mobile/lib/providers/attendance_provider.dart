8148772b9f178b241c38e49f4b4b35bd7d1cb75c
Sumitgujar <sumitgujar1604@gmail.com>
2026-09-24 23:30:13 +0530

    modified attend.provider

--- mobile/lib/providers/attendance_provider.dart
+++ mobile/lib/providers/attendance_provider.dart
@@ -1,7 +1,7 @@
 
-import "package:flutter/material.dart";
-import "package:prok_mobile/models/attendance_model.dart";
-import "package:prok_mobile/services/api_service.dart";
+import 'package:flutter/material.dart';
+import 'package:prok_mobile/models/attendance_model.dart';
+import 'package:prok_mobile/services/api_service.dart';
 
 enum ProviderState { initial, loading, loaded, error }
 
@@ -15,6 +15,8 @@ class AttendanceProvider extends ChangeNotifier {
 
   List<AttendanceSummary> get summary => _summary;
   List<Map<String, dynamic>> get teacherCourses => _teacherCourses;
+  // Alias used by teacher screens
+  List<Map<String, dynamic>> get teacherClasses => _teacherCourses;
   List<Map<String, dynamic>> get enrolledStudents => _enrolledStudents;
   List<Map<String, dynamic>> get courseHistory => _courseHistory;
   ProviderState get state => _state;
@@ -26,44 +28,49 @@ class AttendanceProvider extends ChangeNotifier {
   Future<void> loadSummary() async {
     _state = ProviderState.loading; notifyListeners();
     try {
-      final data = await _api.get("/attendance/student/summary");
-      _summary = (data as List).map((e) => AttendanceSummary.fromJson(e)).toList();
+      final data = await _api.get('/attendance/student/summary');
+      _summary = (data as List).map((e) => AttendanceSummary.fromJson(e as Map<String, dynamic>)).toList();
       _state = ProviderState.loaded;
-    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll("Exception: ", ""); }
+    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll('Exception: ', ''); }
     notifyListeners();
   }
 
   Future<void> loadTeacherCourses() async {
     _state = ProviderState.loading; notifyListeners();
     try {
-      final data = await _api.get("/attendance/teacher/courses");
-      _teacherCourses = List<Map<String, dynamic>>.from(data);
+      final data = await _api.get('/attendance/teacher/courses');
+      _teacherCourses = List<Map<String, dynamic>>.from(data as List);
       _state = ProviderState.loaded;
-    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll("Exception: ", ""); }
+    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll('Exception: ', ''); }
     notifyListeners();
   }
+
+  // Alias for teacher screens
+  Future<void> fetchTeacherClasses() => loadTeacherCourses();
 
   Future<void> loadEnrolledStudents(String courseId) async {
     _state = ProviderState.loading; notifyListeners();
     try {
-      final data = await _api.get("/attendance/courses/$courseId/students");
-      _enrolledStudents = List<Map<String, dynamic>>.from(data);
+      final data = await _api.get('/attendance/courses/\$courseId/students');
+      final raw = data is List ? data : (data as Map)['students'] ?? [];
+      _enrolledStudents = List<Map<String, dynamic>>.from(raw as List);
       _state = ProviderState.loaded;
-    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll("Exception: ", ""); }
+    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll('Exception: ', ''); }
     notifyListeners();
   }
 
   Future<void> loadCourseHistory(String courseId) async {
     _state = ProviderState.loading; notifyListeners();
     try {
-      final data = await _api.get("/attendance/courses/$courseId/history");
-      _courseHistory = List<Map<String, dynamic>>.from(data);
+      final data = await _api.get('/attendance/courses/\$courseId/history');
+      final raw = data is List ? data : (data as Map)['sessions'] ?? [];
+      _courseHistory = List<Map<String, dynamic>>.from(raw as List);
       _state = ProviderState.loaded;
-    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll("Exception: ", ""); }
+    } catch (e) { _state = ProviderState.error; _error = e.toString().replaceAll('Exception: ', ''); }
     notifyListeners();
   }
 
   Future<void> submitAttendance({required String courseId, required List<Map<String, dynamic>> records}) async {
-    await _api.post("/attendance/mark", {"course_id": courseId, "records": records});
+    await _api.post('/attendance/mark', {'course_id': courseId, 'records': records});
   }
 }
